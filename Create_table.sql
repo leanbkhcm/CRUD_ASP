2a6e44298553924712f0c3ef015ca8fb5aee4f74
leanbkhcm <39318027+leanbkhcm@users.noreply.github.com>
2020-03-24 06:15:17 +0700

    Add files via upload

---
+++ Create_table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [Name] VARCHAR(50) NOT NULL,
+    [Title] VARCHAR(50) NOT NULL,
+    [Salary] MONEY NOT NULL,
+)
